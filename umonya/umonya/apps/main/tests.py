1727f1944e2094d9c97c224ca594180db50873ea
Mark Gituma <gitumarkk@gmail.com>
2013-06-13 18:07:05 +0200

    Did PEP8 changes and added unittests
    Changed code structure to pep8 Standards (Thanks @Nina)
    Added docstrings to classes and functions
    Created unittests for page rendering and about page content
    Changed the main.json file location to the more "django like" fixtures
    filename in the main app

--- umonya/umonya/apps/main/tests.py
+++ umonya/umonya/apps/main/tests.py
@@ -1,16 +1,94 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from models import About, Page
+import datetime
+from django.utils.timezone import utc
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class TestPages(TestCase):
+    def test_home(self):
         """
-        Tests that 1 + 1 always equals 2.
+            Tests to see if home page is rendered
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog(self):
+        """
+            Tests to see if blog page is rendered
+        """
+        response = self.client.get("/blog/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        """
+            Tests to see if about page is rendered and ensures that it
+            is the about page based on the response.context
+        """
+        response = self.client.get("/about/")
+        self.assertTrue('about' in response.context)
+        self.assertTrue('page_content' in response.context)
+        self.assertEqual(response.status_code, 200)
+
+    def test_registration(self):
+        """
+            Tests to see if home registration is rendered
+        """
+        response = self.client.get("/registration/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_resources(self):
+        """
+            Tests to see if resource page is rendered
+        """
+        response = self.client.get("/resources/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_course(self):
+        """
+            Tests to see if course page is rendered
+        """
+        response = self.client.get("/course/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact(self):
+        """
+            Tests to see if contact page is rendered
+        """
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+
+
+class TestPageContent(TestCase):
+    def test_about_content(self):
+        """
+            Tests to see if the about page content is rendered correctly
+            from the data base.
+        """
+        page = Page.objects.create(page="about",
+                                   content="<h1>Umonya heading</h1>")
+
+        response = self.client.get("/about/")
+        content = response.context['page_content'][0]
+        self.assertEqual([a.pk for a in response.context['page_content']], [1])
+        self.assertEqual(content.page, "about")
+        self.assertEqual(content.content, "<h1>Umonya heading</h1>")
+
+    def test_about_bios(self):
+        """
+            Tests to see if the bios section of the about page is
+            rendered correctly from the data base
+        """
+        about = About.objects.create(name="Umonya Name",
+                                     role="Umonya Role",
+                                     bios="Umonya Bios",
+                                     bios_photo="path/2/Um/Photo.png",
+                                     pub_date=datetime.datetime.utcnow().
+                                     replace(tzinfo=utc))
+
+        response = self.client.get("/about/")
+        content = response.context['about'][0]
+        self.assertEqual([a.pk for a in response.context['about']], [1])
+        self.assertEqual(content.name, "Umonya Name")
+        self.assertEqual(content.role, "Umonya Role")
+        self.assertEqual(content.bios, "Umonya Bios")
+        self.assertEqual(content.bios_photo, "path/2/Um/Photo.png")
--- umonya/umonya/apps/main/urls.py
+++ umonya/umonya/apps/main/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 #~ Creating the App importable
-urlpatterns	= patterns('',
-	url(r'^$', 'umonya.apps.main.views.home', name='home'),
-	url(r'^about/$', 'umonya.apps.main.views.about'),
-	url(r'^resources/$', 'umonya.apps.main.views.resources'),
-	url(r'^registration/$', 'umonya.apps.main.views.registration'),
-	url(r'^contact/$', 'umonya.apps.main.views.contact'),
-	url(r'^course/$', 'umonya.apps.main.views.course'),
-	url(r'^blog/$', 'umonya.apps.main.views.blog'),
+urlpatterns = patterns('',
+    url(r'^$', 'umonya.apps.main.views.home', name='home'),
+    url(r'^about/$', 'umonya.apps.main.views.about'),
+    url(r'^resources/$', 'umonya.apps.main.views.resources'),
+    url(r'^registration/$', 'umonya.apps.main.views.registration'),
+    url(r'^contact/$', 'umonya.apps.main.views.contact'),
+    url(r'^course/$', 'umonya.apps.main.views.course'),
+    url(r'^blog/$', 'umonya.apps.main.views.blog'),
 )
