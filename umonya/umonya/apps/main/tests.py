14b10f4ab0c2454765bf1d196da0a93cd26ba807
William Stewart <zoidbergwill@gmail.com>
2013-06-19 09:08:08 +0200

    Fixed test to support overflowing days.

--- umonya/umonya/apps/main/tests.py
+++ umonya/umonya/apps/main/tests.py
@@ -1,8 +1,7 @@
+import datetime, calendar
+from django.utils.timezone import utc
 from django.test import TestCase
 from models import About, Page, Announcement
-import datetime
-from django.utils.timezone import utc
-
 
 class TestPages(TestCase):
     def test_home(self):
@@ -102,6 +101,10 @@ class TestIsValidDate(TestCase):
         year = pub_date.year
         month = pub_date.month
         day = pub_date.day + 1
+        if day < 1:
+            day = calendar.monthrange(year, month - 1)[1]
+        elif day > calendar.monthrange(year, month)[1]:
+            day = 1
         event_date = datetime.date(year,month,day)
         test = models.Announcement(pub_date,event_date)
         self.assertEqual(True, test.is_valid_date())
@@ -111,6 +114,10 @@ class TestIsValidDate(TestCase):
         year = pub_date.year
         month = pub_date.month
         day = pub_date.day - 1
+        if day < 1:
+            day = calendar.monthrange(year, month - 1)[1]
+        elif day > calendar.monthrange(year, month)[1]:
+            day = 1
         event_date = datetime.date(year,month,day)
         test = models.Announcement(pub_date,event_date)
         self.assertEqual(False, test.is_valid_date())
